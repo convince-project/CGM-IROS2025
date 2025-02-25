48c467d44ed4e1e966a1ccd3ebb59c5cf702bf80
SofiaFaraci <sofia.faraci@iit.it>
2025-02-25 10:23:47 +0100

    edit topic name batterylevel
    Signed-off-by: SofiaFaraci <sofia.faraci@iit.it>

--- laboratory-tour/src/components/battery_component/src/BatteryComponent.cpp
+++ laboratory-tour/src/components/battery_component/src/BatteryComponent.cpp
@@ -21,7 +21,7 @@ bool BatteryComponent::start(int argc, char*argv[])
                                                                                                        this, 
                                                                                                        std::placeholders::_1)
                                                                                              );
-    m_publisherBatteryState = m_node->create_publisher<std_msgs::msg::Int32>("/battery_level", 10);
+    m_publisherBatteryState = m_node->create_publisher<std_msgs::msg::Int32>("/BatteryComponent/battery_level", 10);
     m_timer = m_node->create_wall_timer(std::chrono::seconds(1), std::bind(&BatteryComponent::BatteryStatePublisherCallback, this));
     RCLCPP_INFO(m_node->get_logger(), "BatteryComponent::start");
     return true;
--- laboratory-tour/src/skills/battery_level_skill/src/BatteryLevelSkill.cpp
+++ laboratory-tour/src/skills/battery_level_skill/src/BatteryLevelSkill.cpp
@@ -67,7 +67,7 @@ bool BatteryLevelSkill::start(int argc, char*argv[])
 
     
     m_subscription_battery_level = m_node->create_subscription<std_msgs::msg::Int32>(
-		"/battery_level", 10, std::bind(&BatteryLevelSkill::topic_callback_battery_level, this, std::placeholders::_1));
+		"/BatteryComponent/battery_level", 10, std::bind(&BatteryLevelSkill::topic_callback_battery_level, this, std::placeholders::_1));
     
 
     
